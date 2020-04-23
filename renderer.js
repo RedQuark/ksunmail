fb2ac9506cca97fd5a32161e975c294c65649333
CAD97 <cad97@cad97.com>
2020-04-23 16:25:07 -0400

    set up ipc

--- renderer.js
+++ renderer.js
@@ -4,3 +4,20 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function doPopTest() {
+    console.log('doPopTest');
+    ipcRenderer.send('do-pop-test', {
+        port: document.getElementById("port").value,
+        host: document.getElementById("host").value,
+        client: {
+            tlserrs: document.getElementById("tlserrs").checked,
+            enabletls: document.getElementById("enabletls").checked,
+            debug: document.getElementById("debug").checked,
+        },
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value,
+    });
+}
+
+document.getElementById("submit").onclick = doPopTest;
